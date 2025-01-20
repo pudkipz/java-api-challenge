837c888f4d1f474ce73539ceb07a16ffa61b33a0
Matilda Sönnergaard <matilda.sonnergaard@se.experis.com>
2025-01-20 15:52:56 +0100

    Initialize project

---
+++ src/main/java/com/booleanuk/java_api_challenge/JavaApiChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.booleanuk.java_api_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaApiChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaApiChallengeApplication.class, args);
+	}
+
+}
